cae9ea19759adb584a361084d5000073e59517f1
Maprangsoft <maprangsoft@gmail.com>
2024-04-18 16:57:45 +0700

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const i = document.createElement("link");
-i.href = "bundle.css",
+i.href = "https://cdn.jsdelivr.net/gh/maprangsoft/test-run-winget@main/bundle.css",
 i.type = "text/css",
 i.rel = "stylesheet",
 
