62c884f5019e107e1dcc8a2ab8c33c129fddad36
maprangsoft <maprangsoft@gmail.com>
2024-04-18 16:48:53 +0700

    1

---
+++ client.js
@@ -0,0 +1,8 @@
+const i = document.createElement("link");
+i.href = "./bundle.css",
+i.type = "text/css",
+i.rel = "stylesheet",
+
+document.getElementsByTagName("head")[0].appendChild(i)
+
+import './bundle.js'
